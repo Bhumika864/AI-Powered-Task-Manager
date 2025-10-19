0c3c2ba97a36aa1f5b24851ea49c819ae35fc7db
Bhumika864 <rajputbhumika121@gmail.com>
2025-10-19 20:17:00 +0530

    created  Header Component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./Components.styles.css"
+
+const Header = ()=>{
+  return (
+  <header className="text-center py-5 bg-blue-600 " >
+    <h1 className="text-3xl font-bold text-white" >Task Manager</h1>
+  </header>
+  )
+}
+export default Header;
